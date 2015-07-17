260e4092b5e886e1d21707a612c2ff3d67b2af98
Carol Manchó <carol.mancho@gmail.com>
2015-07-17 08:45:27 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 
+
+//Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
