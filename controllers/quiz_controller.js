1a7d539dd6284fd623f79d04a1c4fbaae8d9a46e
Carol Manchó <carol.mancho@gmail.com>
2015-07-23 09:16:09 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -26,12 +26,12 @@ exports.index = function(req, res){
     			where: ["pregunta like ?", condicion],
     			order: [['pregunta', 'ASC']]}
     			).then(function(quizes) {
-    			res.render('quizes/index.ejs', {quizes: quizes});
+    			res.render('quizes/index.ejs', {quizes: quizes, errors:[] });
   		  }).catch(function(error) {next(error);});
 	  }else{
       models.Quiz.findAll().then(
         function(quizes) {
-        res.render('quizes/index.ejs', { quizes: quizes});
+        res.render('quizes/index.ejs', { quizes: quizes, errors: [] });
         }
       ).catch(function(error){ next(error); })
 	  }
@@ -39,18 +39,11 @@ exports.index = function(req, res){
 
 
 
-/*exports.index = function(req,res) {
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index', {quizes: quizes});
-	}
-     ).catch(function(error) {next(error);})
-};*/
-
 
 
 
 exports.show = function(req,res) {
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: [] });
 };
 
 
@@ -61,14 +54,46 @@ exports.answer= function(req,res) {
 	if(req.query.respuesta === req.quiz.respuesta) {
 		resultado = 'Correcto';
 	} 
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado });
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: [] });
+};
+
+
+
+exports.new = function(req,res) {
+	var quiz = models.Quiz.build(
+		{pregunta:"Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz, errors: [] });
+};
+
+
+
+
+exports.create = function(req,res) {
+
+	var quiz = models.Quiz.build( req.body.quiz);
+
+	quiz
+	.validate()
+	  .then(
+		function(err) {	
+			if(err) {
+			   res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+			    quiz
+			    .save({fields: ["pregunta","respuesta"]})
+			    .then(function(){ res.redirect('/quizes')})
+			}
+		}	
+	
+	); 
 };
 
 
 
 
 exports.author= function(req,res) {
-	res.render('author', {author: 'Carol Mancho'});
+	res.render('author', {author: 'Carol Mancho', errors: []});
 };
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 //Autoload de comandos con :quizId
@@ -13,9 +13,11 @@ router.param('quizId', quizController.load);
 
 
 //Definicion de rutas de /quizes
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/author', quizController.author);
+router.get('/quizes', 				quizController.index);
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+router.get('/quizes/new', 			quizController.new);
+router.post('/quizes/create', 			quizController.create);
+router.get('/author', 				quizController.author);
 
 module.exports = router;
